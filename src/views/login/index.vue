632fcb911de52f9d96b9b870fa614e8e4b35007b
3050759173@qq.com <3050759173@qq.com>
2022-05-19 05:02:37 +0800

    第一次本地提交注释

---
+++ src/views/login/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="login-container">
+    <el-form :model="form">
+      <el-form-item>
+        <el-input v-model="form.name" ></el-input>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const form = ref({
+  name: ''
+})
+</script>
+
+<style lang="scss" scoped></style>
